d6b58c8be34fee6c6c83466e3c27cbc965a911c4
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-20 17:08:34 -0400

    allow multiple files to be passed on updatePkg

--- index.js
+++ index.js
@@ -4,24 +4,34 @@ var fs = require('fs');
 var path = require('path');
 var utils = require('./utils');
 var cwd = utils.cwd;
+var keys;
 
 /**
  * Update `package.json` with the added file
  */
 
-exports.updatePkg = function(filepath, cb) {
-  if (typeof filepath === 'function') {
-    cb = filepath;
-    filepath = 'utils.js';
+exports.updatePkg = function(files, cb) {
+  if (typeof files === 'function') {
+    cb = files;
+    files = 'utils.js';
   }
 
+  files = arrayify(files);
   var pkg = utils.pkg();
+  var len = files.length, i = -1;
+  var arr = [];
 
-  if (pkg.files.indexOf(filepath) !== -1) {
+  while (++i < len) {
+    if (pkg.files.indexOf(files[i]) > -1) continue;
+    arr.push(files[i]);
+  }
+
+  if (!arr.length) {
     return cb(null, false);
   }
 
-  pkg.files.push(filepath);
+  pkg.files = pkg.files.concat(files);
+  pkg.files.sort();
   utils.writeJson(cwd('package.json'), pkg, function (err) {
     if (err) return console.error(err);
     return cb(null, true);
@@ -57,12 +67,22 @@ exports.addUtils = function(filepath, cb) {
  */
 
 function createUtils(template, pkg) {
-  var keys = Object.keys(pkg.dependencies);
-  var snippet = keys.map(function (key) {
-    return 'require(\'' + key + '\');';
-  }).join('\n');
+  keys || (keys = Object.keys(pkg.dependencies));
+  var len = keys.length, i = -1;
+  var res = [];
 
+  while (++i < len) {
+    var key = keys[i];
+    if (key === 'lazy-cache') continue;
+    res.push('require(\'' + key + '\');');
+  }
+
+  var snippet = res.join('\n');
   return utils.inject(template, snippet, {
     stripTags: true
   });
+}
+
+function arrayify(val) {
+  return Array.isArray(val) ? val : [val];
 }
--- test.js
+++ test.js
@@ -4,6 +4,8 @@ require('mocha');
 var fs = require('fs');
 var assert = require('assert');
 var del = require('delete');
+var writeJson = require('write-json');
+var utils = require('./utils');
 var lazy = require('./');
 
 describe('lazy-cache cli', function () {
@@ -11,9 +13,18 @@ describe('lazy-cache cli', function () {
     lazy.addUtils('foo.js', function (err) {
       if (err) return done(err);
       assert(fs.existsSync('foo.js'));
-      del('foo.js', function (err) {
+      var pkg = utils.pkg();
+      var idx = pkg.files.indexOf('foo.js');
+      assert(idx > -1);
+      pkg.files.splice(idx, 1);
+
+      writeJson('package.json', pkg, function(err) {
         if (err) return done(err);
-        done();
+
+        del('foo.js', function (err) {
+          if (err) return done(err);
+          done();
+        });
       });
     });
   });
